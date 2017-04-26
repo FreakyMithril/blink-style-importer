245a58c1b6befb59e4d7e79b96eedf5c130812a4
Alex Trofymenko <mithrilwork@gmail.com>
2017-04-26 17:07:08 +0300

    removed boostrap to remove errors

--- src/js/main.js
+++ src/js/main.js
@@ -37,6 +37,14 @@
       addStylesOnPage();
     }
   });
+  // // Called when the user clicks on the browser action.
+  // chrome.browserAction.onClicked.addListener(function(tab) {
+  //   // No tabs or host permissions needed!
+  //   console.log('Turning ' + tab.url + ' red!');
+  //   chrome.tabs.executeScript({
+  //     code: 'document.body.style.backgroundColor="red"'
+  //   });
+  // });
 
 })(jQuery, window, document);
 //========================================================================
