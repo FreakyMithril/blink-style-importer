4dbd92342882c32ba20289020a35ed2cea6bc711
Alex Trofymenko <mithrilwork@gmail.com>
2017-05-03 15:55:31 +0300

    put add styles in semarate function

--- src/js/main.js
+++ src/js/main.js
@@ -1,46 +1,43 @@
 document.addEventListener('DOMContentLoaded', function() {
   'use strict';
 
-  let submitStyles = document.getElementById('submitStyles');
-  let textAreaLog = document.getElementById('customCsslog');
-  let currentCssArea = document.getElementById('currentCss');
+  const submitStyles = document.getElementById('submitStyles');
+  const clearStyles = document.getElementById('clearStyles');
+  const textAreaLog = document.getElementById('customCsslog');
+  const currentCssArea = document.getElementById('currentCss');
 
   function notifyMessage(word) {
     textAreaLog.value += word + "\n";
   }
 
-  function mainFunction() {
-
+  function sendStylesToPage(){
     let textAreaHtml = document.getElementById('cssStylesArea');
     let labelForNewCss = document.getElementById('cssStylesLabel');
     let stylesData = textAreaHtml.value;
 
-    function sendStylesToPage(){
-      labelForNewCss.innerHTML = 'Form Submitted';
-      if (!stylesData) {
-        labelForNewCss.innerHTML = 'Invalid text provided';
-        notifyMessage( 'Please put styles');
-      } else {
-        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-          chrome.tabs.sendMessage(tabs[0].id, {greeting: "sendData", data: stylesData}, function(response) {
-            labelForNewCss.innerHTML = 'Changed data in page';
-            notifyMessage('Send data to extension');
-            if (response.success === true) {
-                currentCssArea.value = response.currentData;
-                notifyMessage('Show current styles');
-            } else {
-              notifyMessage('Something wrong');
-            }
-          });
+    labelForNewCss.innerHTML = 'Form Submitted';
+    if (!stylesData) {
+      labelForNewCss.innerHTML = 'Invalid text provided';
+      notifyMessage( 'Please put styles');
+    } else {
+      chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        chrome.tabs.sendMessage(tabs[0].id, {greeting: "sendData", data: stylesData}, function(response) {
+          labelForNewCss.innerHTML = 'Changed data in page';
+          notifyMessage('Send data to extension');
+          if (response.success === true) {
+            currentCssArea.value = response.currentData;
+            notifyMessage('Show current styles');
+          } else {
+            notifyMessage('Something wrong');
+          }
         });
-      }
+      });
     }
-    sendStylesToPage();
   }
 
   submitStyles.onclick = function(event) {
     event.preventDefault();
     notifyMessage('Form Submitted');
-    mainFunction();
+    sendStylesToPage();
   };
 });
