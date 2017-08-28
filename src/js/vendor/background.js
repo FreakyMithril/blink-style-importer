4074ed0b87af14fc5c0bb14901ca7bf41aa90687
Alex Trofymenko <mithrilwork@gmail.com>
2017-08-28 17:54:23 +0300

    start add firebase, also added blank background page, and many not correct things

--- src/js/vendor/background.js
+++ src/js/vendor/background.js
@@ -1,5 +1,5 @@
 let notifyMessage = word => {
-  return false;
+  //return false;
   console.log('Blink extension background script says: ', word);
 };
 
@@ -83,3 +83,37 @@ chrome.extension.onMessage.addListener(function (request, sender) {
     notifyMessage('Icon Disabled');
   }
 });
+
+//start firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyBrXAGQBSqnvkd-DF2yW8TP1SYuQqEaAao",
+  databaseURL: "https://blink-style-importer.firebaseio.com",
+  storageBucket: "blink-style-importer.appspot.com"
+};
+firebase.initializeApp(firebaseConfig);
+
+
+/**
+ * initApp handles setting up the Firebase context and registering
+ * callbacks for the auth status.
+ *
+ * The core initialization is in firebase.App - this is the glue class
+ * which stores configuration. We provide an app name here to allow
+ * distinguishing multiple app instances.
+ *
+ * This method also registers a listener with firebase.auth().onAuthStateChanged.
+ * This listener is called when the user is signed in or out, and that
+ * is where we update the UI.
+ *
+ * When signed in, we also authenticate to the Firebase Realtime Database.
+ */
+function initApp() {
+  // Listen for auth state changes.
+  firebase.auth().onAuthStateChanged(function(user) {
+    notifyMessage('User state change detected from the Background script of the Chrome Extension:', user);
+  });
+}
+
+window.onload = function() {
+  initApp();
+};
